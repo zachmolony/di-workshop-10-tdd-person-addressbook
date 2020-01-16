ba6224d9cfd55020d0153b52430fe3fe7fea9124
zachmolony <zachmolony@deloitte.co.uk>
2020-01-16 11:58:07 +0000

    added init tests

--- test/person.test.js
+++ test/person.test.js
@@ -2,3 +2,18 @@ const chai = require('chai')
 const expect = chai.expect
 
 // Your Person test goes here!
+
+const Person = require('../models/person.js')
+
+describe('Person', () => {
+    it('should init', () => {
+        var person = new Person('Zach', 'Molony', '09/07/2001')
+
+        expect(person.first).to.equal('Zach')
+        expect(person.last).to.equal('Molony')
+        expect(person.dob).to.equal('09/07/2001')
+
+        expect(person.email).to.deep.equal([])
+    })
+
+})
